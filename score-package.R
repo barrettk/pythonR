09a21b58530b2fe7b467b0b6986224f5bc75243d
Kyle Barrett <barrettk@metrumrg.com>
2024-02-07 20:59:05 -0500

    function to score package using mpn.scorecard
     - added to build ignore

---
+++ score-package.R
@@ -0,0 +1,52 @@
+
+score_package <- function(
+    risk_breaks = c(0.3, 0.7),
+    open_dirs = TRUE,
+    overwrite = TRUE
+){
+
+  # Setup Directories
+  score_dir <- tempfile(pattern = "score_dir")
+  fs::dir_create(score_dir)
+
+  pkg_dir <- file.path(score_dir, "pkg_dir")
+  out_dir <- file.path(score_dir, "out_dir")
+  fs::dir_create(pkg_dir)
+  fs::dir_create(out_dir)
+
+  # Build package
+  tar_file <- devtools::build(pkg = ".", path = pkg_dir, quiet = TRUE)
+
+  # Score package
+  results_dir <- mpn.scorecard::score_pkg(
+    pkg = tar_file, out_dir = out_dir,
+    overwrite = overwrite
+  )
+
+  # Traceability Matrix
+  trac_matrix <- make_traceability_matrix(
+    tar_file, results_dir = results_dir
+  )
+
+  # Scorecard
+  pdf_path <- mpn.scorecard::render_scorecard(
+    results_dir,
+    risk_breaks = risk_breaks,
+    overwrite = overwrite,
+    add_traceability = "auto"
+  )
+
+
+  # Open results directory and browse PDF
+  if(isTRUE(open_dirs)){
+    rstudioapi::filesPaneNavigate(score_dir)
+    browseURL(pdf_path)
+  }
+
+  return(
+    list(
+      score_dir = score_dir,
+      trac_matrix = trac_matrix
+    )
+  )
+}
